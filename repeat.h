7b41554d203fda99681cd57cb73e8ccec2c97b29
Awes Mubarak <contact@awesmubarak.com>
2021-03-27 18:16:05 +0000

    Add initial structure to repeat program
    Adds the functions to the header file and basic string parsing.

--- repeat.h
+++ repeat.h
@@ -5,7 +5,13 @@
 #include "program.h"
 
 //--------Teammember 2 to complete this section (class declaration) ----------
-
+class repeat {
+private:
+    // TODO: how do I add the program as an attribute
+public:
+    void run();
+    friend void operator>>(istream& in);
+}
 //----------------------------------------------------------------------------
 
 #endif /* REPEAT_H_ */
