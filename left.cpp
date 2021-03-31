a4a73825dafc430c841599f23184e2fb25e21457
tzhen1 <tzhen80@gmail.com>
2021-03-31 10:45:09 +0100

    added class definition to movements actions

--- left.cpp
+++ left.cpp
@@ -4,3 +4,12 @@
 //--------Teammember 3 to complete this section (class definition) ----------
 
 //----------------------------------------------------------------------------
+void Left::run()
+{
+
+}
+
+std::istream& operator>>(std::istream& in, Left& l)
+{
+
+}
--- left.h
+++ left.h
@@ -6,5 +6,13 @@
 //--------Teammember 3 to complete this section (class declaration) ----------
 
 //----------------------------------------------------------------------------
+class Left: public Command //inherits public funcs, command.h
+{
+public:
+    void run(); // virtual func run 
+    friend std::istream& operator>>(std::istream& in, Left& l); //input operator
+
+
+};
 
 #endif /* LEFT_H_ */
