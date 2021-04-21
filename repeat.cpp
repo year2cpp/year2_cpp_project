ec8ee7c3471ef966e40964a02be77f0ab66cc805
Awes Mubarak <contact@awesmubarak.com>
2021-04-21 10:07:36 +0100

    Attempt to make it work

--- repeat.cpp
+++ repeat.cpp
@@ -1,18 +1,22 @@
-/*#include "repeat.h"
+#include "repeat.h"
 
 //--------Teammember 2 to complete this section (class definition) ----------
+
+void run() {
+    string output_line = "";
+    output_line >> Prog;
+}
+
 void operator>>(istream& in) {
     const int max_in_size = 100;
-    String input_line;
+    string input_line;
     int n_repeats;
     char program[max_in_size];
 
-    std::getline(std::cin, input_line);
-    std::sscanf(input_line, "REPEAT %d [%99s", &n_repeats, program);
+    getline(cin, input_line);
+    sscanf(input_line, "REPEAT %d [%99s", &n_repeats, program);
 
     for (size_t n_repeats = 0; n_repeats < count; n_repeats++) {
         program >> prog;
     }
 }
-
-*/
