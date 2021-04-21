b616e92de0921190de7ef687241a9208b193d27c
robvasistha <robvasistha@gmail.com>
2021-04-21 16:46:00 +0100

    Updated to now use command vector

--- program.cpp
+++ program.cpp
@@ -2,77 +2,93 @@
 #include "program.h"
 
 
-void Program::run() {
+void Program::run(){
 	//--------Teammember 1 to replace this section----------
+	for (int i = 0; i < cmds.size(); i++)
+	{
+		cmds.at(i);
+	}
+	
+	
+	
+	
+	/*
+	-----------------------------------------------------------------------------------
 	if(str == "FORWARD"){
 		// find number associated with the instruction, call function that number of times
-		forwardObj.run();
+		
 	}
-	else if (str == "LEFT"){
-		leftObj.run();
+	else if (cmds == "LEFT"){
+		
 	}
 	else if (str == "RIGHT"){
-		rightObj.run();
+		
 	}
 	else if (str == "JUMP"){
-		jumpObj.run();
+		
 	}
 	else if (str == "REPEAT"){
 		//see whats inside the brackets and perform x times
 		if(str == "FORWARD"){
-			forwardObj.run();
+			
 		}
 		else if (str == "LEFT"){
-			leftObj.run();
+			
 		}
 		else if (str == "RIGHT"){
-			rightObj.run();
+			
 		}
 		else if (str == "JUMP"){
-			jumpObj.run();
+			
 		}
 	}
 	else{
 		std::cout << "Invalid input instruction" << std::endl;
-	}
+	}*/
 
 	//-------------------------------------------------------
-	//below is prereplace with ifs and using tom and awes' parts.
-	/*
-	glBegin(GL_LINE_LOOP);								Draw the 3 connected vertices of a triangle
-		glVertex3f( 0.0f, 1.0f, 0.0f);					Top
-		glVertex3f(-1.0f,-1.0f, 0.0f);					Bottom Left
-		glVertex3f( 1.0f,-1.0f, 0.0f);					Bottom Right
-	glEnd();											Finished Drawing The Triangle
-
-	glTranslatef(-2.5f,0.0f,0.0f);						Move Left 1.5 Units
-	glRotatef(20,0,0,1);								Rotate by 20 degrees
-	glBegin(GL_LINE_LOOP);								Draw the triangle again
-		glVertex3f( 0.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f,-1.0f, 0.0f);
-		glVertex3f( 1.0f,-1.0f, 0.0f);
-	glEnd();
-
-	glRotatef(-20,0,0,1);								Rotate back again
-	glTranslatef(5.0f,0.0f,0.0f);						Move Right 5 Units
-	glBegin(GL_LINE_LOOP);								Draw a square
-		glVertex3f(-1.0f, 1.0f, 0.0f);					Top Left
-		glVertex3f( 1.0f, 1.0f, 0.0f);					Top Right
-		glVertex3f( 1.0f,-1.0f, 0.0f);					Bottom Right
-		glVertex3f(-1.0f,-1.0f, 0.0f);					Bottom Left
-	glEnd();*/
 }
 
 std::istream& operator>>(std::istream& in, Program& prog)
 {
 	//--------Teammember 1 to complete this section----------
+	Command* left = new Command;
+	Command* right = new Command;
+	Command* jump = new Command;
+	Command* forward = new Command;
 	while (in.peek() == '\n'){//looks until next line is found.
 		in.get();//retrieves all the characters before the new line
+		std::getline(in,prog.str, ' ');//gets all the characters before the space and puts into prog.str
+		in >> prog.distance;//puts the rest into prog.distance.
+		if (prog.str == "FORWARD"){
+			for (int i = 0; i < prog.distance; i++)
+			{
+				prog.cmds.push_back(forward);
+			}
+		}
+		else if (prog.str == "JUMP"){
+			for (int i = 0; i < prog.distance; i++)
+			{
+				prog.cmds.push_back(jump);
+		}
+		}
+		else if (prog.str == "LEFT"){
+			for (int i = 0; i < prog.distance; i++)
+			{
+				prog.cmds.push_back(left);
+			}
+		}
+		else if (prog.str == "RIGHT"){
+			for (int i = 0; i < prog.distance; i++)
+			{
+				prog.cmds.push_back(right);
+			}
+		}
 	}
-	std::getline(in,prog.str, ' ');//gets all the characters before the space and puts into prog.str
-	std::cout << "This is the string that istream gets: " << prog.str << std::endl;
-	in >> prog.distance;//puts the rest into prog.distance.
-	std::cout << "This is the distance: "<< prog.distance << std::endl;
+	//while(in.good()){ not sure if I need this but peipei suggested it.
+
+
+	//}
 	return in;//returns the istream.
 	//-------------------------------------------------------
 }
--- program.h
+++ program.h
@@ -11,22 +11,26 @@
 #include "forward.h"
 #include "jump.h"
 
-class Program {
+class Program : public Command {
 
 public:
-	void run();
+	void run() override;
 	//--------Teammember 1 to complete this section (class declaration) ----------
 	std::string str;
 	float distance;
 	/*Repeat repeatObj;*/
-	Forward forwardObj;
+	/*Forward forwardObj;
 	Left leftObj;
 	Right rightObj;
-	Jump jumpObj;
+	Jump jumpObj;*/
+	~Program();
+	friend std::istream& operator>>(std::istream&, Program& prog);
 	//----------------------------------------------------------------------------
 
-};
 
+private:
+	std::vector<Command*> cmds;
+};
 
 
 std::istream& operator>>(std::istream& in, Program& prog);
