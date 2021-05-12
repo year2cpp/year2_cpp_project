58f7fb7e1e1535aee3f5b40e3cd9cbf28f95fe9d
robvasistha <robvasistha@gmail.com>
2021-05-12 23:21:58 +0100

    attempt at error handling

--- program.cpp
+++ program.cpp
@@ -14,41 +14,47 @@ Program::~Program(){
 
 std::istream &operator>>(std::istream &in, Program &prog)
 {
+	if(!in.good()){
+		std::cerr << "Erroneous input" << std::endl;
+	}
 	while (in.good())
-	{									
+	{					
+				
 		in >> prog.str;
-		if (prog.str == "FORWARD")
-		{
-			Forward *obj = new Forward();
-			in >> *obj;
-			prog.cmds.push_back(obj);
-		}
-		else if (prog.str == "JUMP")
-		{
-			Jump *obj = new Jump();
-			in >> *obj;
-			prog.cmds.push_back(obj);
-		}
-		else if (prog.str == "LEFT")
-		{
-			Left *obj = new Left();
-			in >> *obj;
-			prog.cmds.push_back(obj);
-		}
-		else if (prog.str == "RIGHT")
-		{
+			if (prog.str == "FORWARD")
+			{
+				Forward *obj = new Forward();
+				in >> *obj;
+				prog.cmds.push_back(obj);
+				
+			}
+			else if (prog.str == "JUMP")
+			{
+				Jump *obj = new Jump();
+				in >> *obj;
+				prog.cmds.push_back(obj);
+			}
+			else if (prog.str == "LEFT")
+			{
+				Left *obj = new Left();
+				in >> *obj;
+				prog.cmds.push_back(obj);
+			}
+			else if (prog.str == "RIGHT")
+			{
 
-			Right *obj = new Right();
-			
-			in >> *obj;
-			prog.cmds.push_back(obj);
-		}
-		else if (prog.str == "REPEAT")
-		{
-			Repeat *obj = new Repeat();
-			in >> *obj;
-			prog.cmds.push_back(obj);
-		}
+				Right *obj = new Right();
+				
+				in >> *obj;
+				prog.cmds.push_back(obj);
+			}
+			else if (prog.str == "REPEAT")
+			{
+				Repeat *obj = new Repeat();
+				in >> *obj;
+				prog.cmds.push_back(obj);
+			}
+		
 	}
 	return in; 
 }
