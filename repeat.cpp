8e1bf714f4fc4307f66b4443a58b5025da21fa0d
robvasistha <robvasistha@gmail.com>
2021-05-12 11:09:57 +0100

    V1 working

--- repeat.cpp
+++ repeat.cpp
@@ -3,28 +3,14 @@
 //--------Teammember 2 to complete this section (class definition) ----------
 
 void Repeat::run(){
-    for (int i = 0; i < value ; i++) {
-        prog.run();
-    }
-}
-
-<<<<<<< HEAD
-void operator>>(istream& in) {
-    std::string tmpInputVar; //for square bracket in beginnging
-    in >> rpt.value >> tmpInputVar;
-    in >> rpt.prog;
-
-}
-=======
-*/
-#include "repeat.h"
-
-void Repeat::run(){
-
-
+ for (int i = 0; i < value ; i++) {
+ prog.run();
+ }
 }
 
 std::istream& operator>>(std::istream& in, Repeat& rpt){
-
+ std::string tmpInputVar; //for square bracket in beginnging
+ in >> rpt.value >> tmpInputVar;
+ in >> rpt.prog;
+ return in;
 }
->>>>>>> 4bb20596182e038d8433f2ec104426a9cf3067a9
--- repeat.h
+++ repeat.h
@@ -7,13 +7,8 @@
 class Repeat: public Command{
  Program prog;
 public:
-<<<<<<< HEAD
-    void run();
-    friend std::istream& operator>>(std::istream& in, Repeat& rpt);
-=======
  void run();
  friend std::istream& operator>>(std::istream& in, Repeat& rpt);
->>>>>>> 4bb20596182e038d8433f2ec104426a9cf3067a9
 };
 
 
